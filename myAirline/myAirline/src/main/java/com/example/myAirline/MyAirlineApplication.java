5c0a1320686ff6ce32d09862a9015ce111e6a023
Florin <f.schikarski@tu-bs.de>
2022-11-07 11:11:15 +0100

    spring initzlr

---
+++ myAirline/myAirline/src/main/java/com/example/myAirline/MyAirlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.myAirline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAirlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAirlineApplication.class, args);
+	}
+
+}
---
+++ myAirline/myAirline/src/test/java/com/example/myAirline/MyAirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myAirline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAirlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

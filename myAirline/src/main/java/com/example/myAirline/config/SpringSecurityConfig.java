6f188f403589c383d289227d8a0e24ba66c9eda3
Florin <f.schikarski@tu-bs.de>
2022-11-12 00:03:45 +0100

    feat: minor changes

--- myAirline/src/main/java/com/example/myAirline/config/SpringSecurityConfig.java
+++ myAirline/src/main/java/com/example/myAirline/config/SpringSecurityConfig.java
@@ -29,7 +29,7 @@ public class SpringSecurityConfig {
 
 
     @Bean
-    public PasswordEncoder encode() {
+    public PasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder(30);
     }
